4c48883634359dd66f9346e108f1b1b4b4703404
Tessakam <tessa.kam@hotmail.com>
2020-10-02 10:57:03 +0200

    mentally preparing for exercise 5

--- expert.php
+++ expert.php
@@ -2,8 +2,8 @@
 
 //Displaying errors - no console.log in PHP
 declare(strict_types=1);
-ini_set('display_errors', "on");
-ini_set('display_startup_errors', "on");
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
 // Below are several code blocks, read them, understand them and try to find whats wrong.
@@ -17,21 +17,51 @@ error_reporting(E_ALL);
 // sometimes, even your IDE can tell you what's wrong
 echo "Exercise 1 starts here:";
 
-function new_exercise()
+function new_exercise($x) //DONE: add the parameter to the function to define
 {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
-    return $block; // add return
 }
 
-new_exercise(2);
+new_exercise(2); //DONE: an array starts with 0
 // === Exercise 2 ===
 // Below we create a week array with all days of the week.
 // We then try to print the first day which is monday, execute the code and see what happens.
 
 $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-$monday = $week[0]; // an array starts with 0
+$monday = $week[0];
 
 echo $monday;
+
+new_exercise(3); //DONE: use single quotes
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = 'Debugged !Also very fun';
+echo substr($str, 0, 10);
+
+new_exercise(4); //DONE: added & to include the day substr
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+print_r($week);
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+
+$arr = [];
+for ($letter = 'a'; $letter <= 'z'; $letter++) {
+    array_push($arr, $letter);
+}
+
+print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+
 
 ?>
